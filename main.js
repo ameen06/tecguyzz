e427353478712fce644306b7fa655f57c1d8254e
Ameen <ameename406@gmail.com>
2021-06-05 15:30:47 +0530

    New version with Animation

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 function openNav() {
@@ -7,14 +6,4 @@ function openNav() {
 
 function closeNav() {
     document.getElementById('mySidenav').style.width = 0;
-=======
-
-
-function openNav() {
-    document.getElementById('mySidenav').style.width = '80%';
-}
-
-function closeNav() {
-    document.getElementById('mySidenav').style.width = 0;
->>>>>>> 4ed9c6ad3f71f56290623f9e79de7a2371e35365
 }
